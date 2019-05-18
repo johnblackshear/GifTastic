043877ce779997f59c2c11c63166c061b9550f07
John Blackshear <swjaybee@gmail.com>
2019-05-18 17:15:30 -0500

    init

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,38 @@
-$(document).ready(function(){//I need jquery to run!
+$(document).ready(function () {//I need jquery to run!
+	var searchTerms = ['fox', 'cow', 'chicken', 'pig', 'lizard','cat'];
 
-	console.log("testing...");
+	var createButton = function (name) {
+		var button = $('<button>');
+		button.text(name);
 
+		button.click(function () {
+			gifQuery($(this).text());
+		});
 
+		$('.button-container').append(button);
+	}
 
+	var createButtonsFromArray = function (arr) {
+		for (var i = 0; i < arr.length; i++) {
+			createButton(arr[i]);
+		}
+	}
+
+	var gifQuery = function (term) {
+		var apiKey = "vdJbozY65dSnXQCZUu0q59cvKTdKhVMF";
+		var queryURL = "http://api.giphy.com/v1/gifs/search?apiKey="
+			+ apiKey
+			+ "&q=" + term;
+
+		$.ajax({
+			method: "GET",
+			url: queryURL,
+		}).then(function (result) {
+			console.log(result);
+		});
+	}
+
+	gifQuery("cat");
+
+	createButtonsFromArray(searchTerms);
 });
